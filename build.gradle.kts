de30ea0e2d3df5f8d72981320370c44963dfac2c
Tom Martin <tom.martin1239@gmail.com>
2025-12-09 23:46:52 +0000

    Update dasm to 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
-    implementation("io.github.notstirred:dasm:3.0.0")
+    implementation("io.github.notstirred:dasm:3.1.0")
 
     testImplementation(libs.junit)
     testImplementation(libs.opentest4j)
